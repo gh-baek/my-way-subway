abf9d6a8773e9432cca64a7ab25f5fa13d338893
GH Baek <backtobaek9922@gmail.com>
2023-10-11 14:04:51 +0900

    프로젝트 프로토 타입

---
+++ lib/functions.dart
@@ -0,0 +1,127 @@
+import 'dart:io';
+
+import 'package:dijkstra/dijkstra.dart';
+import 'package:excel/excel.dart';
+
+late final Set stationSet; // 전체 역 종류
+late final List<List> pairList; // 역 간 그래프
+late final Graph timeGraph; // 역 간 소요 시간 그래프
+late final Graph distGraph; // 역 간 거리 그래프
+late final Graph costGraph; // 역 간 비용 그래프
+
+//역(노드)로 구성된 그래프
+class Graph {
+  final Map<dynamic, Map> nodes;
+
+  Graph(this.nodes);
+}
+
+//역 정보 및 그래프를 받아오는 함수
+//앱 실행 최초에만 실행하여 각 정보 전역 변수에 저장
+void getStationInfo() {
+  var file = 'lib/data/stations.xlsx';
+  var bytes = File(file).readAsBytesSync();
+  var excel = Excel.decodeBytes(bytes);
+
+  for (var table in excel.tables.keys) {
+    // print(table); //sheet Name
+    // print(excel.tables[table]?.maxCols);
+    // print(excel.tables[table]?.maxRows);
+    int maxRows = excel.tables[table]!.maxRows; //엑셀에 저장된 행(거리, 비용, 시간 역간 정보) 개수
+    List stations = []; //(역1, 역2, 시간(초), 거리(미터), 비용(원))
+
+    for (var i = 1; i < maxRows; i++) {
+      Map data = {
+        'st1': 0,
+        'st2': 0,
+        'time': 0,
+        'dist': 0,
+        'cost': 0,
+      };
+      data['st1'] = excel.tables[table]?.rows[i][0]!.value;
+      data['st2'] = excel.tables[table]?.rows[i][1]!.value;
+      data['time'] = excel.tables[table]?.rows[i][2]!.value;
+      data['dist'] = excel.tables[table]?.rows[i][3]!.value;
+      data['cost'] = excel.tables[table]?.rows[i][4]!.value;
+      stations.add(data);
+    }
+
+    // 모든 역 입렵 -> set 형식이므로 중복은 제거
+    stationSet = {};
+    for (var i = 0; i < maxRows - 1; i++) {
+      stationSet.add(stations[i]['st1']);
+      stationSet.add(stations[i]['st2']);
+    }
+
+    // 역 간 연결 정보를 저장
+    pairList = [];
+    for (var i = 0; i < maxRows - 1; i++) {
+      pairList.add([stations[i]['st1'], stations[i]['st2']]);
+    }
+    Map<dynamic, Map> timeNodes = {};
+    Map<dynamic, Map> distNodes = {};
+    Map<dynamic, Map> costNodes = {};
+
+    //역 간 소요 시간에 대한 timeGraph 생성 for문
+    for (var st in stationSet) {
+      Map data = {};
+      for (var i = 0; i < maxRows - 1; i++) {
+        if (stations[i]['st1'] == st) {
+          data[stations[i]['st2']] = stations[i]['time'];
+        } else if (stations[i]['st2'] == st) {
+          data[stations[i]['st1']] = stations[i]['time'];
+        }
+      }
+      timeNodes[st] = data;
+    }
+    timeGraph = Graph(timeNodes);
+    //print(timeGraph.nodes);
+
+    //역 간 거리에 대한 distGraph 생성 for문
+    for (var st in stationSet) {
+      Map data = {};
+      for (var i = 0; i < maxRows - 1; i++) {
+        if (stations[i]['st1'] == st) {
+          data[stations[i]['st2']] = stations[i]['dist'];
+        } else if (stations[i]['st2'] == st) {
+          data[stations[i]['st1']] = stations[i]['dist'];
+        }
+      }
+      distNodes[st] = data;
+    }
+    distGraph = Graph(distNodes);
+
+    //역 간 비용에 대한 costGraph 생성 for문
+    for (var st in stationSet) {
+      Map data = {};
+      for (var i = 0; i < maxRows - 1; i++) {
+        if (stations[i]['st1'] == st) {
+          data[stations[i]['st2']] = stations[i]['cost'];
+        } else if (stations[i]['st2'] == st) {
+          data[stations[i]['st1']] = stations[i]['cost'];
+        }
+      }
+      costNodes[st] = data;
+    }
+    costGraph = Graph(costNodes);
+    // print(costGraph.nodes);
+  }
+}
+
+// 길 찾기 함수: 파라미터는 departure(출발역)와 arrival(도착역)
+void findBestWay({required int departure, required int arrival}) {
+  var output = Dijkstra.findPathFromPairsList(pairList, departure, arrival);
+  var timeOutput =
+      Dijkstra.findPathFromGraph(timeGraph.nodes, departure, arrival);
+  var distOutput =
+      Dijkstra.findPathFromGraph(distGraph.nodes, departure, arrival);
+  var costOutput =
+      Dijkstra.findPathFromGraph(costGraph.nodes, departure, arrival);
+
+  print("best time output:");
+  print(timeOutput);
+  print("best distance output:");
+  print(distOutput);
+  print("best cost output:");
+  print(costOutput);
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:material_floating_search_bar_2/material_floating_search_bar_2.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    BorderRadiusGeometry radius = BorderRadius.only(
+      topLeft: Radius.circular(24.0),
+      topRight: Radius.circular(24.0),
+    );
+    return Scaffold(
+      body: SlidingUpPanel(
+        minHeight: 100,
+        borderRadius: radius,
+        panel: Center(
+          child: Text("This is the sliding Widget"),
+        ),
+        body: Stack(
+          children: [
+            PhotoView(
+              imageProvider: AssetImage("lib/assets/images/map.png"),
+              initialScale: PhotoViewComputedScale.covered,
+            ),
+            buildFloatingSearchBar(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildFloatingSearchBar() {
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+
+    return FloatingSearchBar(
+      hint: 'Search...',
+      scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+      transitionDuration: const Duration(milliseconds: 800),
+      transitionCurve: Curves.easeInOut,
+      physics: const BouncingScrollPhysics(),
+      axisAlignment: isPortrait ? 0.0 : -1.0,
+      openAxisAlignment: 0.0,
+      width: isPortrait ? 600 : 500,
+      debounceDelay: const Duration(milliseconds: 500),
+      onQueryChanged: (query) {
+        // Call your model, bloc, controller here.
+      },
+      // Specify a custom transition to be used for
+      // animating between opened and closed stated.
+      transition: CircularFloatingSearchBarTransition(),
+      actions: [
+        FloatingSearchBarAction(
+          showIfOpened: false,
+          child: CircularButton(
+            icon: const Icon(Icons.place),
+            onPressed: () {},
+          ),
+        ),
+        FloatingSearchBarAction.searchToClear(
+          showIfClosed: false,
+        ),
+      ],
+      builder: (context, transition) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Material(
+            color: Colors.white,
+            elevation: 4.0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: Colors.accents.map((color) {
+                return Container(height: 112, color: color);
+              }).toList(),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
